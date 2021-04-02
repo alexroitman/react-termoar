957cfacaedc6a5ba5dc30fea99a284e221760249
Roitman <alexroitman99@gmail.com>
2021-04-02 12:13:25 -0300

    initial commit

---
+++ precache-manifest.b2db51715e23e4c51cd9942f5afa3116.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344f124f000a95254c1709f48c50e2ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e68abbba48995cd54b9",
+    "url": "/static/css/2.00668387.chunk.css"
+  },
+  {
+    "revision": "c216071319767281a0f3",
+    "url": "/static/css/main.7dbe15a8.chunk.css"
+  },
+  {
+    "revision": "1e68abbba48995cd54b9",
+    "url": "/static/js/2.7fe4d07a.chunk.js"
+  },
+  {
+    "revision": "980b85962847aa8967aae5074f7fd5d5",
+    "url": "/static/js/2.7fe4d07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c216071319767281a0f3",
+    "url": "/static/js/main.c6b802ec.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "d28fad14a7b1aa5f9fc99d15bc937784",
+    "url": "/static/media/Banner Home 1.d28fad14.png"
+  },
+  {
+    "revision": "076ac82ba757369926fc8a0e7d7b0838",
+    "url": "/static/media/Banner Home 2.076ac82b.png"
+  },
+  {
+    "revision": "445dbfc7a50583ef2c53d5a2b00e92c9",
+    "url": "/static/media/Banner Home 3.445dbfc7.png"
+  },
+  {
+    "revision": "efdeab472dec9cc57c9b7c21ec91cd0a",
+    "url": "/static/media/Certificado DNV V 2022.efdeab47.pdf"
+  },
+  {
+    "revision": "aca649d8857c9f80a3dda864215c8570",
+    "url": "/static/media/Foto Empresa1.aca649d8.png"
+  },
+  {
+    "revision": "8a9c18e2754d2e19c676c372b28bf532",
+    "url": "/static/media/Foto Empresa2.8a9c18e2.png"
+  },
+  {
+    "revision": "0c86921175ffdd7ab994b45054c45fe9",
+    "url": "/static/media/NuestroLab.0c869211.png"
+  },
+  {
+    "revision": "af0c024f8e9addd322e90486fddcd160",
+    "url": "/static/media/Politica de calidad 2019.af0c024f.pdf"
+  },
+  {
+    "revision": "94b9c57b7ed0176c39366c4c5103f102",
+    "url": "/static/media/Servicio1.94b9c57b.png"
+  },
+  {
+    "revision": "1c8a2641d5948f9c69c33db2328a6927",
+    "url": "/static/media/Servicio2.1c8a2641.png"
+  },
+  {
+    "revision": "838482d2e2e3641b13534a9009db7627",
+    "url": "/static/media/Servicio3.838482d2.png"
+  },
+  {
+    "revision": "322db76c023f212f8dc0d14afb26ee99",
+    "url": "/static/media/Servicio4.322db76c.png"
+  },
+  {
+    "revision": "fde1f2a75c9389caadc62a6919f5e047",
+    "url": "/static/media/Servicio5.fde1f2a7.png"
+  },
+  {
+    "revision": "28dbcba38542ea89895ce51843f428a3",
+    "url": "/static/media/Servicio6.28dbcba3.png"
+  },
+  {
+    "revision": "389898bd48f4f7ca4a3f3acde1f56e82",
+    "url": "/static/media/Servicio7.389898bd.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "55418b4820f1a8665b6e0531672edc40",
+    "url": "/static/media/banner.55418b48.jpg"
+  },
+  {
+    "revision": "75dc502c5fcc8fdc99128b9b44806491",
+    "url": "/static/media/banner2.75dc502c.jpg"
+  },
+  {
+    "revision": "3da8a2f347f20c1ef93aaaf964eff975",
+    "url": "/static/media/banner3.3da8a2f3.jpg"
+  },
+  {
+    "revision": "8a10f5e474d3b22da75e797820dc4ee0",
+    "url": "/static/media/computacion-en-la-nube.8a10f5e4.png"
+  },
+  {
+    "revision": "b415dba3c0c59599945b6b40d02fd8ab",
+    "url": "/static/media/confianza.b415dba3.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "722ed91e378e172c7b83eb90240d0d10",
+    "url": "/static/media/foto-home.722ed91e.png"
+  },
+  {
+    "revision": "efd263bb4078b069ab5ac2e9033a9768",
+    "url": "/static/media/iso.efd263bb.png"
+  },
+  {
+    "revision": "d3f7d18fcfabd2f5cbf901b751dc22f8",
+    "url": "/static/media/logo termo pared.d3f7d18f.png"
+  },
+  {
+    "revision": "c3871af41d701227e025d7374e018037",
+    "url": "/static/media/medidor.c3871af4.png"
+  },
+  {
+    "revision": "7637ea1322b7cefe4eac307c47037080",
+    "url": "/static/media/termometro.7637ea13.png"
+  }
+]);
